9d6d59cad30c6da5e5f382fd21523f8c71939a19
mefreanzy <92319842+mefreanzy@users.noreply.github.com>
2026-07-31 13:28:37 +0300

    Update stage2.asm

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,10 +1,26 @@
 [org 0x1000]
+bits 16
 
 mov ah, 0x0e
 mov al, 'S'
 int 0x10
 mov al, '2'
 int 0x10
+
+A20:
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+ProtectMode:
+    cli
+    lgdt [GDTDescriptor]
+
+    mov eax, cr0
+    or eax, 0x01
+    mov cr0, eax
+
+    jmp 0x08:Stage32
 
 align 4
 GDT:
@@ -27,10 +43,25 @@ GDTData:
     db 0xCF
     db 0x00
 
-GTDEnd:
+GDTEnd:
 GDTDescriptor:
     dw GDTEnd - GDT - 1
     dd GDT
 
+bits 32
+Stage32:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000    
 
-jmp $
+    mov edx, 0xB8000
+    mov al, 'K'
+    mov ah, 0x4F
+    mov [edx], ax
+
+
+    jmp $
